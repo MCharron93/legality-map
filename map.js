4252de13232f6c652caaf71fc997739a62929058
MCharron93 <michelle.charron93@gmail.com>
2021-03-22 16:05:52 -0600

    added media queries

--- map.js
+++ map.js
@@ -14,7 +14,6 @@ window.onload = function () {
         "stroke-miterlimit": "4",
         "stroke-width": "0.75",
         "stroke-dasharray": "none",
-        // "viewBox": "0 0 1000 600"
       },
       usRaphael = {};
 
